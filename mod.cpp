191c6ac9f98c2a7299c0b2a3a2bef351059ca93b
isopharius <isopharius@gmail.com>
2016-08-17 21:56:07 -0400

    add mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "LHD Airboss";
+picture = "";
+actionName = "Website";
+action = "https://github.com/isopharius/airboss";
+overview = "Airboss provides Logistics and traffic control for the CUP LHD";
+tooltip = "LHD Airboss";
+author = "isopharius";
