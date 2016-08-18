1c8c60045d3a6cc37df38ba8dac9d6e251305509
isopharius <isopharius@gmail.com>
2016-08-17 23:06:15 -0400

    config info

--- addons/airboss/config.cpp
+++ addons/airboss/config.cpp
@@ -16,6 +16,19 @@ class CfgPatches
 	};
 };
 
+class CfgMods {
+	class PREFIX {
+		dir = "@airboss";
+		name = "LHD Airboss";
+		picture = "";
+		actionName = "Website";
+		action = "https://github.com/isopharius/airboss";
+		overview = "Airboss provides Logistics and traffic control for the CUP LHD";
+		tooltip = "LHD Airboss";
+		author = "isopharius";
+	};
+};
+
 #include "dialog.hpp"
 
 class CfgFunctions
